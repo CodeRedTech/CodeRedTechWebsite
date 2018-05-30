9eae86b01d99eae276fc664ff091c180f826a235
Samantha Smith <samantha.coderedtech@gmail.com>
2018-05-30 15:17:17 -0400

    files

---
+++ js/script.js
@@ -0,0 +1,107 @@
+//twitter
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+
+var works = [
+  {
+    title: "Carmen Meadows Website",
+    pic: "http://wrestler-inclination-16121.bitballoon.com/img2/car1.jpg"
+  },
+  {
+    title: "Web Calculator",
+    pic: "http://infallible-dijkstra-74bd9a.bitballoon.com/img/calculator.png"
+
+  },
+  {
+    title: "Third Project",
+    pic: "http://www.coderedtech.net/img/codingpx.jpg"
+  },
+  {
+    title: "Fourth Project",
+    pic: "http://www.coderedtech.net/img/codingpx.jpg"
+  }
+];
+
+$(document).ready(function(){
+  // Smooth scrolling
+  var $root = $('html, body');
+
+  $('.navbar-nav a').click(function() {
+    var href = $.attr(this, 'href');
+
+    if (href != undefined && href != '#') {
+      $root.animate({
+        scrollTop: $(href).offset().top
+      }, 500, function () {
+        window.location.hash = href;
+      });
+    }
+
+    return false;
+  });
+
+  // parallax effect
+  $.stellar();
+
+  // Tooltips
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  });
+
+  //submit button
+  $('#button').on('click', function() {
+    var comment = $('.message-box').val();
+    $('#visible-comment').html(comment);
+    if ('comment' == '')
+      $('comment').css("border", "red");
+    else { $('comment').hide();
+    };
+
+    return false;
+  });
+
+  //character count
+  $(".message-box").on("keyup", function() {
+    var charCount = $(".message-box").val().length;
+    console.log(charCount);
+    $("#char-count").html(charCount);
+    if (charCount > 50) {
+      $("#char-count").css("color", "red");
+    } else {
+      $("#char-count").css("color", "black");
+    }
+  });
+
+  // work section
+  for(var i = 0; i < works.length; ++i ) {
+    $("#work").append("\
+    <div class='col-sm-6 col-md-3'>\
+    <a href='#' class='work-img'>\
+    <img class='img-responsive' src='" + works[i].pic + "'>\
+    <span class='info'><p class='proj-title'>Title:</p> [WORK TITLE] </span>\
+  </div>\
+");
+  };
+
+  $(".work-img").mouseenter( function() {
+    $(".info", this).show();
+  }).mouseleave(function(){
+    $(".info", this).hide();
+  });
+
+  var images = $("#work img");
+  console.log(images); //checking code
+
+  if(i%2 === 0){
+    $(images[i]).css("border", "3px solid DodgerBlue");
+  } else {
+    $(images[i]).css("border", "3px solid salmon")
+  };
+
+  var map;
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+  }
+});
---
+++ js/work.js
@@ -0,0 +1,26 @@
+var works = [ 
+    
+  {
+    title: "My Project",
+    pic: "img/car1.jpg"
+    url: "http://wrestler-inclination-16121.bitballoon.com/"
+  },
+    
+  {
+    title: "Second Project",
+    pic: "http://www.coderedtech.net/img/codingpx.jpg"
+      
+  },
+    
+  {
+    title: "Third Project",
+    pic: "http://www.animal-photography.com/thumbs/silver_tabby_kittens~AP-0JO6Y9-TH.jpg"
+  },
+    
+  {
+    title: "Fourth Project",
+    pic: "http://www.animal-photography.com/thumbs/silver_tabby_kitten_looking_up~AP-0DLVMB-TH.jpg"
+  }
+];
+
+
